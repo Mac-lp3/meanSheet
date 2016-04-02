5771d3d80bb0c798a4e91711e21e58d84bce812b
Mac-lp3 <staticlp3@gmail.com>
2016-04-02 18:13:45 -0400

    refactored work item API to use /projects and /tasks

---
+++ routes/tasks.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var testData = require('../test/datastore/testData');
+var moment = require('moment');
+var async = require('async');
+var router = express.Router();
+
+/* get work items page. */
+router.get('/', function(req, res, next) {
+
+  // TODO - pagination? Just a subset?
+  res.json(testData.testTasks);
+});
+
