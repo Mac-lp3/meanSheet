22725cdc55079966832988084eb40f16e14682df
Mac-lp3 <staticlp3@gmail.com>
2016-04-16 10:30:28 -0400

    wired login and sign up forms to their controller models

--- public/javascripts/app/login.js
+++ public/javascripts/app/login.js
@@ -1,3 +1,17 @@
 meanApp.controller('LoginController', ['$http', function($http) {
+
+	var self = this;
+
+	// models for login form
+	self.emailAddress = '';
+	self.password = '';
+	
+	// models for sign up form
+	self.formFirstName = '';
+	self.formLastName = '';
+	self.formEmailAddress = '';
+	self.formPassword = '';
+	self.formConfirmPassword = '';
+
 	
 }]);
