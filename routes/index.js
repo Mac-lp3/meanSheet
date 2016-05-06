b7c6f527b2ef4d47bc548caae8f12b5b1f2ad71c
Mac-lp3 <staticlp3@gmail.com>
2016-05-06 09:12:42 -0400

    changes to the dev login form. added new dev login controller.

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,21 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
-/* GET dash page. */
+/* GET login page. */
 router.get('/login', function(req, res, next) {
 
     // TODO environment variables to send proper home page 
     // TODO streams / piping
 
     res.sendFile(path.join(__dirname, '/../public/html/login-dev.html'));
+});
+
+/* POST login form. */
+router.post('/login', function(req, res, next) {
+
+    // res.sendFile(path.join(__dirname, '/../public/html/login-dev.html'));
+    const email = req.body.emailAddress;
+    console.log(email);
 });
 
 /* GET home page. */
