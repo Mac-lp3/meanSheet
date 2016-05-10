c39654fe62072b37726c13f4a24fa62def6e1218
Mac-lp3 <staticlp3@gmail.com>
2016-05-09 20:05:38 -0400

    more structure changes to meet angular standards

--- public/app/app.js
+++ public/app/app.js
@@ -3,17 +3,14 @@
 // Declare app level module which depends on views, and components
 const meanApp = angular.module('meanApp', [
   'ngRoute',
-  'meanControllers'
+  'LoginController',
+  'DashboardController'
 ]);
 
 meanApp.config(['$routeProvider', function($routeProvider) {
 
     // Is there a way for this to be env-dependent?
     $routeProvider.
-      when('/index', {
-        templateUrl: 'dashboard/dashboard.html',
-        controller: 'DashboardController'
-      }).
       otherwise({
         redirectTo: '/login/login-dev.html'
     });
