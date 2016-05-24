57321d0899533a83ab03cbaef63f9d2fa8b75e9e
Mac-lp3 <staticlp3@gmail.com>
2016-05-24 19:27:47 -0400

    moved root scope variable population into seperate angular run method

--- public/app/app.js
+++ public/app/app.js
@@ -8,6 +8,20 @@ const meanApp = angular.module('meanApp', [
   'addWorkItemService'
 ]);
 
+meanApp.run(function($rootScope) {
+	
+    $rootScope.TASK_WORK_ITEM_TYPE = 'Task';
+    $rootScope.PROJECT_WORK_ITEM_TYPE = 'Project';
+    $rootScope.LEAVE_WORK_ITEM_TYPE = 'Leave';
+
+    $rootScope.URL_MAPPINGS = {
+    	'Task' : 'tasks',
+    	'Project' : 'projects',
+    	'Leave' : 'leave'
+    };
+
+});
+
 meanApp.config(['$routeProvider', function($routeProvider) {
 
     // Is there a way for this to be env-dependent?
